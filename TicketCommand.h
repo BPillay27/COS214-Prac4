4693ba52550e1423c3caea5e7e8dad151922a2e8
ByronPillay <byronpillay2704@gmail.com>
2025-10-06 11:22:55 +0200

    TicketCommand head done

---
+++ TicketCommand.h
@@ -0,0 +1,14 @@
+#ifndef TICKETCOMMAND_H
+#define TICKETCOMMAND_H
+
+#include <string>
+
+class TicketCommand
+{
+public:
+    TicketCommand();
+    virtual void execute(std::string id, std::string info) = 0;
+    virtual std::string getType() = 0;
+};
+
+#endif

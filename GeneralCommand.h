7a6e33ccfdbd3cbd1a1d60c72fc3dabdc4191e2c
ByronPillay <byronpillay2704@gmail.com>
2025-10-06 11:34:15 +0200

    Headers for tech,gen and fin command

---
+++ GeneralCommand.h
@@ -0,0 +1,14 @@
+#ifndef TECHCOMMAND_H
+#define TECHCOMMAND_H
+
+#include "TicketCommand.h"
+
+class GeneralCommand : public TicketCommand
+{
+public:
+    GeneralCommand();
+    void execute(std::string id, std::string info);
+    std::string getType();
+};
+
+#endif

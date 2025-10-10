e354b2ed91d2703800b591d2edb114be6302b254
BPillay27 <byronpillay2704@gmail.com>
2025-10-10 16:15:43 +0200

    Completed TicketSystemMaker implementation

--- TicketSystemMaker.cpp
+++ TicketSystemMaker.cpp
@@ -4,49 +4,94 @@
 #include "FinanceCommand.h"
 #include "TechCommand.h"
 #include "GeneralCommand.h"
+#include "TicketCategory.h"
 
 void TicketSystemMaker::reset()
 {
+    TicketCategory *temp = new TicketCategory("root");
 
+    if (root == NULL)
+    {
+        root = temp;
+        return;
+    }
+
+    temp->add(root);
+    root = temp;
 }
 
 void TicketSystemMaker::addCategory(std::string name)
 {
+    if (root == NULL)
+    {
+        root = new TicketCategory(name);
+        return;
+    }
 
+    root->add(new TicketCategory(name));
 }
 
 void TicketSystemMaker::addFinanceTicket(std::string id, std::string info)
 {
-    TicketLeaf* temp=new TicketLeaf(id,info);
+    TicketLeaf *temp = new TicketLeaf(id, info);
     temp->setCmd(new FinanceCommand());
+
+    if (root == NULL)
+    {
+        root = temp;
+        return;
+    }
     currentCategory->add(temp);
 }
 
 void TicketSystemMaker::addTechTicket(std::string id, std::string info)
 {
-    TicketLeaf* temp=new TicketLeaf(id,info);
+    TicketLeaf *temp = new TicketLeaf(id, info);
     temp->setCmd(new TechCommand());
+
+    if (root == NULL)
+    {
+        root = temp;
+        return;
+    }
     currentCategory->add(temp);
 }
 
 void TicketSystemMaker::addGeneralTicket(std::string id, std::string info)
 {
-    TicketLeaf* temp=new TicketLeaf(id,info);
+    TicketLeaf *temp = new TicketLeaf(id, info);
     temp->setCmd(new GeneralCommand());
+
+    if (root == NULL)
+    {
+        root = temp;
+        return;
+    }
     currentCategory->add(temp);
 }
 
-TicketSystem *getTicketSystem()
+TicketSystem *TicketSystemMaker::getTicketSystem()
 {
-    
+    return root;
 }
 
-TicketSystemMaker::TicketSystemMaker()
+TicketSystemMaker::TicketSystemMaker() : SystemMaker()
 {
-
+    root = NULL;
+    currentCategory = NULL;
 }
 
 TicketSystemMaker::~TicketSystemMaker()
 {
+    if (currentCategory != NULL)
+    {
+        delete currentCategory;
+        currentCategory = NULL;
+    }
 
+    if (root != NULL)
+    {
+        delete root;
+        root = NULL;
+    }
 }
